60de06f89104cbc1f11f339deaf91e79a8ada242
Dmytro Mostovyi <me@dmostovyi.com>
2025-02-02 13:50:29 +0100

    Update Nixfmt package

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -11,132 +11,150 @@
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, rust-overlay }:
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+      rust-overlay,
+    }:
     let
-      configuration = { pkgs, ... }: {
-        nixpkgs.config.allowUnfree = true;
+      configuration =
+        { pkgs, ... }:
+        {
+          nixpkgs.config.allowUnfree = true;
 
-        # List packages installed in system profile. To search by name, run:
-        # $ nix-env -qaP | grep wget
-        environment.systemPackages = with pkgs; [
-          _1password-cli
-          nixfmt
-          nixd
-          neovim
-          tmux
-          go
-          tinygo
-          elixir
-          starship
-          git
-          delta
-          fnm
-          fzf
-          eza
-          htop
-          nano
-          mkcert
-          jq
-          wget
-          zoxide
-          zellij
-          ripgrep
-          lazygit
-          lazydocker
-          rust-bin.stable.latest.default
-          pnpm
-          bun
-          deno
-          zig
-          uv
-          curlie
-        ];
-
-        fonts = {
-          packages = with pkgs; [ jetbrains-mono nerd-fonts.jetbrains-mono ];
-        };
-
-        homebrew = {
-          enable = true;
-          brews = [ "gleam" ];
-          casks = [
-            "1password@7"
-            "meetingbar"
-            "orbstack"
-            "setapp"
-            "virtualbox@beta"
-            "yaak"
-            "adobe-acrobat-reader"
-            "firefox"
-            "languagetool"
-            "mockoon"
-            "rectangle"
-            "sublime-text"
-            "visual-studio-code"
-            "zed"
-            "nvidia-geforce-now"
-            "ghostty"
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = with pkgs; [
+            _1password-cli
+            nixfmt-rfc-style
+            nixd
+            neovim
+            tmux
+            go
+            tinygo
+            elixir
+            starship
+            git
+            delta
+            fnm
+            fzf
+            eza
+            htop
+            nano
+            mkcert
+            jq
+            wget
+            zoxide
+            zellij
+            ripgrep
+            lazygit
+            lazydocker
+            rust-bin.stable.latest.default
+            pnpm
+            bun
+            deno
+            zig
+            uv
+            curlie
           ];
-          masApps = { "nordvpn" = 905953485; };
 
-          onActivation = {
-            autoUpdate = true;
-            upgrade = true;
-            cleanup = "zap";
+          fonts = {
+            packages = with pkgs; [
+              jetbrains-mono
+              nerd-fonts.jetbrains-mono
+            ];
           };
-        };
 
-        security.pam.enableSudoTouchIdAuth = true;
-
-        system = {
-          defaults = {
-            dock = {
-              tilesize = 50;
-              autohide = true;
-              mru-spaces = false;
-              static-only = true;
-              show-recents = false;
-              expose-group-apps = false;
+          homebrew = {
+            enable = true;
+            brews = [ "gleam" ];
+            casks = [
+              "1password@7"
+              "meetingbar"
+              "orbstack"
+              "setapp"
+              "virtualbox@beta"
+              "yaak"
+              "adobe-acrobat-reader"
+              "firefox"
+              "languagetool"
+              "mockoon"
+              "rectangle"
+              "sublime-text"
+              "visual-studio-code"
+              "zed"
+              "nvidia-geforce-now"
+              "ghostty"
+            ];
+            masApps = {
+              "nordvpn" = 905953485;
             };
-            finder = {
-              AppleShowAllFiles = true;
-              AppleShowAllExtensions = true;
-              FXPreferredViewStyle = "clmv";
+
+            onActivation = {
+              autoUpdate = true;
+              upgrade = true;
+              cleanup = "zap";
             };
           };
-        };
 
-        # Auto upgrade nix package and the daemon service.
-        services.nix-daemon.enable = true;
-        nix = {
-          package = pkgs.nix;
-          settings = {
-            "extra-experimental-features" = [ "nix-command" "flakes" ];
+          security.pam.enableSudoTouchIdAuth = true;
+
+          system = {
+            defaults = {
+              dock = {
+                tilesize = 50;
+                autohide = true;
+                mru-spaces = false;
+                static-only = true;
+                show-recents = false;
+                expose-group-apps = false;
+              };
+              finder = {
+                AppleShowAllFiles = true;
+                AppleShowAllExtensions = true;
+                FXPreferredViewStyle = "clmv";
+              };
+            };
           };
-          nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          nix = {
+            package = pkgs.nix;
+            settings = {
+              "extra-experimental-features" = [
+                "nix-command"
+                "flakes"
+              ];
+            };
+            nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+          };
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Create /etc/zshrc that loads the nix-darwin environment.
+          programs.zsh.enable = true; # default shell on catalina
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs = {
+            overlays = [ rust-overlay.overlays.default ];
+            hostPlatform = "aarch64-darwin";
+          };
         };
-
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.zsh.enable = true; # default shell on catalina
-        # programs.fish.enable = true;
-
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 5;
-
-        # The platform the configuration will be used on.
-        nixpkgs = {
-          overlays = [ rust-overlay.overlays.default ];
-          hostPlatform = "aarch64-darwin";
-        };
-      };
-    in {
+    in
+    {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#mac
       darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
