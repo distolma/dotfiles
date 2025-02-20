0e8089d25cb2a4dd9028b18600c9c122e66e2580
Dmytro Mostovyi <me@dmostovyi.com>
2025-02-20 13:05:02 +0100

    Replace neo-tree with snacks explorer

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+---@module 'snacks'
+
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      sources = { explorer = {
+        layout = { layout = { position = "right" } },
+      } },
+    },
+  },
+}
